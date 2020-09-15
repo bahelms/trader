0d2f11d820dd0ac29489d5ae66f47e9426423a41
Barrett Helms <barrett.helms@salesloft.com>
2020-09-14 21:26:28 -0400

    Provides symbol as cli argument

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 mod apis;
 mod config;
 
+use std::env;
+
 use apis::td_ameritrade;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Must provide a symbol to use");
+        return;
+    }
+
+    let symbol = args[1].to_uppercase();
     let env = config::init_env();
     let mut tda_client = td_ameritrade::client(&env);
 
-    let symbol = "SQQQ";
-    for candle in tda_client.price_history(symbol) {
+    for candle in tda_client.price_history(&symbol) {
         println!("{}: {}", symbol, candle);
     }
 }
