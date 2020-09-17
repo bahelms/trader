2149009ebc7ab1ba78eac5998bb5082db02cfe6f
Barrett Helms <barrett.helms@salesloft.com>
2020-09-17 07:23:23 -0400

    Moves backtest reporting into the struct

--- src/main.rs
+++ src/main.rs
@@ -28,45 +28,5 @@ fn main() {
     }
 
     trades = strategies::sma9_crossover(&candles, trades);
-    log_results(trades);
-}
-
-fn log_results(trades: Backtest) {
-    let mut winning_trades = Vec::new();
-    let mut losing_trades = Vec::new();
-    for position in &trades.positions {
-        if position.total_return() >= 0.0 {
-            winning_trades.push(position);
-        } else {
-            losing_trades.push(position);
-        }
-    }
-    let mut winning_returns: Vec<f64> = winning_trades.iter().map(|p| p.total_return()).collect();
-    let mut losing_returns: Vec<f64> = losing_trades.iter().map(|p| p.total_return()).collect();
-    let total_wins: f64 = winning_returns.iter().sum();
-    let total_losses: f64 = losing_returns.iter().sum();
-    winning_returns.sort_by(|a, b| b.partial_cmp(a).unwrap());
-    losing_returns.sort_by(|a, b| a.partial_cmp(b).unwrap());
-
-    println!(
-        "Trades won: {} - total returns: ${}",
-        winning_trades.len(),
-        total_wins
-    );
-    println!(
-        "Trades lost: {} - total returns: ${}",
-        losing_trades.len(),
-        total_losses
-    );
-    println!(
-        "Win %: {:.2}",
-        winning_trades.len() as f64 / trades.positions.len() as f64 * 100.0
-    );
-    println!(
-        "Highest return: ${} - Lowest return: ${}",
-        winning_returns[0], losing_returns[0],
-    );
-
-    println!("Net return: ${}", total_wins + total_losses);
-    println!("Ending Capital: ${}", trades.capital);
+    trades.log_results();
 }
--- src/trading.rs
+++ src/trading.rs
@@ -48,6 +48,47 @@ impl Backtest {
             positions: Vec::new(),
         }
     }
+
+    pub fn log_results(&self) {
+        let mut winning_trades = Vec::new();
+        let mut losing_trades = Vec::new();
+        for position in &self.positions {
+            if position.total_return() >= 0.0 {
+                winning_trades.push(position);
+            } else {
+                losing_trades.push(position);
+            }
+        }
+        let mut winning_returns: Vec<f64> =
+            winning_trades.iter().map(|p| p.total_return()).collect();
+        let mut losing_returns: Vec<f64> = losing_trades.iter().map(|p| p.total_return()).collect();
+        let total_wins: f64 = winning_returns.iter().sum();
+        let total_losses: f64 = losing_returns.iter().sum();
+        winning_returns.sort_by(|a, b| b.partial_cmp(a).unwrap());
+        losing_returns.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        println!(
+            "Trades won: {} - total returns: ${}",
+            winning_trades.len(),
+            total_wins
+        );
+        println!(
+            "Trades lost: {} - total returns: ${}",
+            losing_trades.len(),
+            total_losses
+        );
+        println!(
+            "Win %: {:.2}",
+            winning_trades.len() as f64 / self.positions.len() as f64 * 100.0
+        );
+        println!(
+            "Highest return: ${} - Lowest return: ${}",
+            winning_returns[0], losing_returns[0],
+        );
+
+        println!("Net return: ${}", total_wins + total_losses);
+        println!("Ending Capital: ${}", self.capital);
+    }
 }
 
 impl Trades for Backtest {
